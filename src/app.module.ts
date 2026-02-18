09d5018610b5492b45d59e563ed6ec2c0134ae5b
Amay Dixit <amaydixit11@gmail.com>
2026-02-18 23:14:46 +0530

    chore: wire modules and configure vercel deployment

---
+++ src/app.module.ts
@@ -0,0 +1,46 @@
+import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProxyModule } from './proxy/proxy.module';
+import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
+import { MarketplaceModule } from './marketplace/marketplace.module';
+import { AcademicsModule } from './academics/academics.module';
+import { JobsModule } from './jobs/jobs.module';
+import { HomeModule } from './home/home.module';
+import { HealthModule } from './health/health.module';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { AnnouncementsModule } from './announcements/announcements.module';
+import { OrganizationsModule } from './organizations/organizations.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ProxyModule,
+    AuthModule,
+    EventsModule,
+    AnnouncementsModule,
+    OrganizationsModule,
+    MarketplaceModule,
+    AcademicsModule,
+    JobsModule,
+    HomeModule,
+    HealthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ResponseInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,30 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('OpenLake Hub API')
+    .setDescription('The integration backbone of the OpenLake ecosystem')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
+}
+bootstrap();
